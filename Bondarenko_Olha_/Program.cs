ebda71d4aa26cfe9bfbb3f1db1a1d343bdc0ade0
lyolyabond <olha.bondarenko@nure.ua>
2021-09-29 22:34:17 +0300

    Add project files.

---
+++ Bondarenko_Olha_/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bondarenko_Olha_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello!");
+            Console.ReadKey();
+        }
+    }
+}
